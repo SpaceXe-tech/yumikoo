070bb0597410195701f9707ce1b65f7dc215cff2
SpaceXe-tech <billaxe299@gmail.com>
2025-06-17 23:45:20 +0530

    Update stats.py

--- Venom/modules/stats.py
+++ Venom/modules/stats.py
@@ -1,12 +1,15 @@
 from pyrogram import filters, Client
 from pyrogram.types import Message
-
-from Venom import OWNER, VenomX
+from Venom import VenomX, config
 from Venom.database.chats import get_served_chats
 from Venom.database.users import get_served_users
 
+# Authorized users
+OWNER_ID = config.OWNER_ID
+SUDO_ID = config.SUDO_ID
+AUTHORIZED_USERS = [OWNER_ID] + (SUDO_ID if isinstance(SUDO_ID, list) else [SUDO_ID])
 
-@VenomX.on_message(filters.command("stats") & filters.user(OWNER))
+@VenomX.on_message(filters.command("stats") & filters.user(AUTHORIZED_USERS))
 async def stats(cli: Client, message: Message):
     users = len(await get_served_users())
     chats = len(await get_served_chats())
